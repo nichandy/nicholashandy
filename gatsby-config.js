b9a292778dd7293c3eb56b510a431a0baf09d259
Nick Handy <nichandy@gmail.com>
2021-02-03 00:19:32 -0800

    adds content for projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Nicholas Handy Portfolio`,
-        short_name: `Nicholas Handy Portfolio`,
+        name: `nicholashandy.com`,
+        short_name: `nicholashandy.com`,
         description: `A portfolio website to convey experience in engineering and software development`,
         start_url: `/`,
         background_color: `#6b37bf`,
@@ -32,7 +32,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/static/favicon.ico`, // This path is relative to the root of the site.
+        icon: `favicon.ico`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
